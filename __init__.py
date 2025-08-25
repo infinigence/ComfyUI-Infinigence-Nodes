f29fdb4a7bf0d7b67ff876755e501f4caa5a0e41
a5704607 <2667164034@qq.com>
2025-08-25 11:41:18 +0800

    update

--- __init__.py
+++ __init__.py
@@ -2,17 +2,13 @@ from .nodes import *
 from .nodes_0 import *
 
 NODE_CLASS_MAPPINGS = {
-    "DrawTextNode": DrawTextNode
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "DrawTextNode": "DrawTextNode"
-}
-
-NODE_CLASS_MAPPINGS = {
+    "DrawTextNode": DrawTextNode,
     "Qwen2.5VL_api": Qwen2VL_api,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "DrawTextNode": "DrawTextNode",
     "Qwen2.5VL_api": "Qwen2.5VL_api",
 }
+
+
